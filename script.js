37d6dceb72f5b4796a2fb8027f82c54bef7da5a3
natyaraujo <146364738+natyaraujo@users.noreply.github.com>
2025-05-30 10:50:59 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ const textoResultado = document.querySelector(".texto-resultado");
 
 const perguntas = [
     {
-        enunciado: "Assim que saiu da escola você se depara com uma nova tecnologia, um chat que consegue responder todas as dúvidas que uma pessoa pode ter, ele também gera imagens e áudios hiper-realistas. Qual o primeiro pensamento?",
+        enunciado: "Assim que você sair a escola você conseguiu entrar na faculdade que você quer.",
         alternativas: [
             "Isso é assustador!",
             "Isso é maravilhoso!"
